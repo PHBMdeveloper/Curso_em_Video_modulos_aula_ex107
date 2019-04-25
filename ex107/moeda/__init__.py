3dfacb7edf85760a14e6db772de112e5b6d5bc65
PHDeveloper <phbm@hotmail.com>
2019-04-25 01:08:50 -0300

    Aula + Exercício 107

---
+++ ex107/moeda/__init__.py
@@ -0,0 +1,14 @@
+def metade(n):
+    return n / 2
+
+
+def dobro(n):
+    return n * 2
+
+
+def aumentar(n, per):
+    return n + ((n / 100) * per)
+
+
+def diminuir(n, per):
+    return n - ((n / 100) * per)
